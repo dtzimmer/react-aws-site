5ddde1454c4b91c870af07ba666349745e73ce1f
Unknown <daniel.zimmerman@gmail.com>
2018-08-09 16:23:45 -0600

    First Commit. Trying to use S3 bucket to host static website.

---
+++ src/App.js
@@ -0,0 +1,29 @@
+import React, {Fragment, Component} from 'react';
+import './App.css';
+
+class App extends Component {
+    render() {
+        return (
+            <Fragment>
+                <header>
+                    <h1>Hello World</h1>
+                </header>
+                <body>
+                <section>
+                    <div>
+                        <p>Here is some content</p>
+                    </div>
+                    <div>
+                        <p>Here is some more content</p>
+                    </div>
+                    <div>
+                        <p>Here is some more content</p>
+                    </div>
+                </section>
+                </body>
+            </Fragment>
+        );
+    }
+}
+
+export default App;
